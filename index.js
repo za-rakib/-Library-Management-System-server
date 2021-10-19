d2df9aaf377c46ac980f8ae4088b326b550f897e
MD Rakib Hossen <phpzarakib@gmail.com>
2021-10-19 06:30:18 +0600

    books get

--- index.js
+++ index.js
@@ -15,10 +15,14 @@ const client = new MongoClient(uri, {
 });
 
 client.connect((err) => {
+  //Book collection
   const booksCollection = client.db("ist-library").collection("books");
+  // Book Issue collection
   const issueBooksCollection = client
     .db("ist-library")
     .collection("issueBooks");
+  // librarian collection
+  const librarianCollection = client.db("ist-library").collection("librarian");
 
   // books get
   app.get("/getBooks", (req, res) => {
@@ -26,7 +30,13 @@ client.connect((err) => {
       res.send(books);
     });
   });
-
+  
+  //issue book get
+  app.get("/getIssueBooks", (req, res) => {
+    issueBooksCollection.find().toArray((err, issueBooks) => {
+      res.send(issueBooks);
+    });
+  });
   // books post
   app.post("/addBook", (req, res) => {
     const newBook = req.body;
@@ -40,16 +50,35 @@ client.connect((err) => {
   //issueBooks post
   app.post("/issueBook", (req, res) => {
     const issueBook = req.body;
-     console.log("Issue Book", issueBook);
-     issueBooksCollection.insertOne(issueBook).then((result) => {
-       console.log("Book Issue ", result.acknowledged);
-       res.send(result.acknowledged > 0);
-     })
-  })
+    console.log("Issue Book", issueBook);
+    issueBooksCollection.insertOne(issueBook).then((result) => {
+      console.log("Book Issue ", result.acknowledged);
+      res.send(result.acknowledged > 0);
+    });
+  });
+
+  // Department books
+  app.post("/booksByDepartment", (req, res) => {
+    // console.log(req);
+    const department = req.body.departmentName;
+    // console.log("Department Name", department);
+    booksCollection.find({ department }).toArray((err, books) => {
+      // console.log("books", books);
+      res.send(books);
+    });
+  });
+
+  app.post("/addLibrarian", (req, res) => {
+    const librarian = req.body;
+    librarianCollection.insertOne(librarian).then((result) => {
+      console.log("librarian ", result.acknowledged);
+      res.send(result.acknowledged > 0);
+    });
+  });
 });
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send(" Welcome to IST library");
 });
 
 app.listen(process.env.PORT || port);
